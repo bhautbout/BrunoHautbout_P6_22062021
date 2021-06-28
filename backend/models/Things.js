94d38097baef329aa742290e5ffc903561c7a4f7
Bruno Hautbout <bruno.hautbout@gmail.com>
2021-06-28 09:24:47 +0200

    creation des fichiers

---
+++ backend/models/Things.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const thingSchema = mongoose.Schema({
+    
+    name: {type: String, required: true},
+    manufacturer: {type: String, required: true},
+    description: {type: String, required: true},
+    mainPepper: {type: String, required: true},
+    imageUrl: {type: String, required: true},
+    heat: {type: Number, min:1, max: 10, required: true},
+    likes: {type: Number, required: true},
+    dislikes: {type: Number, required: true},
+    // usersLiked: [string],
+    // usersDisliked: [string],
+});
+
+module.exports = mongoose.model('Things', thingSchema);
