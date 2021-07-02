ada5feeeda6ff7f4017ef53ab39ab7019f7cd9fb
Bruno Hautbout <bruno.hautbout@gmail.com>
2021-07-02 12:13:04 +0200

    modification de la route post pour les images

--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -1,13 +1,16 @@
 const Thing = require('../models/Things');
 
 exports.createThing = (req, res, next) => {
-    const thing = new Thing({
-      ...req.body
-    });
-    thing.save()
-      .then(() => res.status(201).json ({ message: 'Objet enregistré !'}))
-      .catch(error => res.status(400).json({ error }));
-  };
+  const thingObject = JSON.parse(req.body.thing);
+  delete thingObject._id;
+  const thing = new Thing({
+    ...thingObject,
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+  });
+  thing.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+    .catch(error => res.status(400).json({ error }));
+};
 
   exports.modifyThing = (req, res, next) => {
     Thing.updateOne({ _id: req.params.id }, {...req.body, _id: req.params.id})
